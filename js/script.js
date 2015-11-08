88a3adfc5c5d051a3f998142ab3479046b8b389f
deglier <degliersa@gmail.com>
2015-11-08 21:27:24 -0200

    mudança radical
    implementado a troca de cores associada ao atributo 'data-bg' em cada
    link do menu

--- js/script.js
+++ js/script.js
@@ -1,42 +1,12 @@
 $(document).ready(function(){
-   $('.home').hover(
-       function(){
-           //entrada
-           $('.menu').addClass('home-hover');
-       },
-       function(){
-           //saida
-           $('.menu').removeClass('home-hover');
-       }); 
-    
-   $('.port').hover(
-       function(){
-           //entrada
-           $('.menu').addClass('port-hover');
-       },
-       function(){
-           //saida
-           $('.menu').removeClass('port-hover');
-       })
-   
-   $('.blog').hover(
-       function(){
-           //entrada
-           $('.menu').addClass('blog-hover');
-       },
-       function(){
-           //saida
-           $('.menu').removeClass('blog-hover');
-       })
-   
-   $('.contato').hover(
-       function(){
-           //entrada
-           $('.menu').addClass('contato-hover');
-       },
-       function(){
-           //saida
-           $('.menu').removeClass('contato-hover');
-       })
+    $("a").hover(
+        function(){
+            var         cor = $(this).attr("data-bg"),
+                estiloHover = {'background-color' : cor};
+            $("nav.menu").css(estiloHover);
+        },
+        function(){
+            $("nav").css("background-color", "#000");
+        });
 
 });
